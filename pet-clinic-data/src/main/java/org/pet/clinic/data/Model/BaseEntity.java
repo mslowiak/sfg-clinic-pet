f15c3fcf643d4a537876141aab8a573e85783c1f
Himovic <chabannehichem@gmail.com>
2019-08-27 20:49:05 +0200

    Creating new BaseEntity Class for generating ID. Closes #10

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Model/BaseEntity.java
@@ -0,0 +1,24 @@
+package org.pet.clinic.data.Model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+	
+	private Long id;
+	
+	public BaseEntity() {
+		
+	}
+	public BaseEntity(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
--- pet-clinic-data/src/main/java/org/pet/clinic/data/Model/PetType.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Model/PetType.java
@@ -1,6 +1,6 @@
 package org.pet.clinic.data.Model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 	
 	private String name;
 	
