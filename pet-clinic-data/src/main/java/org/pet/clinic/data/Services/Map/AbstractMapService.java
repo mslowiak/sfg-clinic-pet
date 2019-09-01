1c78a5404fc2e0b84a931502489a8cfc2c53fa1a
Himovic <chabannehichem@gmail.com>
2019-09-01 11:52:28 +0200

    Refactor Map Implementation class to abstract ID generation. Closes #24

--- pet-clinic-data/src/main/java/org/pet/clinic/data/Services/Map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Services/Map/AbstractMapService.java
@@ -1,13 +1,17 @@
 package org.pet.clinic.data.Services.Map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T,ID> {
+import org.pet.clinic.data.Model.BaseEntity;
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
 	
-	public Map<ID,T> map = new HashMap<>();  
+	public Map<Long,T> map = new HashMap<>();  
 	
 	Set<T> findAll(){
 		return new HashSet<>(map.values());
@@ -17,8 +21,16 @@ public abstract class AbstractMapService<T,ID> {
 		return map.get(id);
 	}
 	
-	T save(ID id,T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(object != null) {
+			if(object.getId() == null) {
+				object.setId(getNextID());
+			}
+			map.put(object.getId(), object);
+		}else {
+			throw new RuntimeException("Object cannot be null");
+		}
+		
 		return object;
 	}
 	
@@ -28,5 +40,16 @@ public abstract class AbstractMapService<T,ID> {
 	
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	
+	/* Manage Setting of ID Property */
+	private Long getNextID() {
+		Long nextID = null;
+		try {
+			nextID = Collections.max(map.keySet())+1;
+		}catch(NoSuchElementException ex) {
+			nextID = 1L;
+		}
+		return nextID;
 	}
 }
--- pet-clinic-web/src/main/java/org/pet/clinic/web/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/pet/clinic/web/Bootstrap/DataLoader.java
@@ -25,13 +25,11 @@ public class DataLoader implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Hichem");
 		owner1.setLastName("CHABANE");
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Mehdi");
 		owner2.setLastName("Lebrima");
 		ownerService.save(owner2);
@@ -39,13 +37,11 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Loaded owners...");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Aymen");
 		vet1.setLastName("CHABANE");
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Ahlem");
 		vet2.setLastName("Teniou");
 		vetService.save(vet2);
