40d4a3ca89e8d0f319f935ba32ed4bf3e79394af
Himovic <chabannehichem@gmail.com>
2019-09-20 16:42:09 +0200

    Create PetType Map Service Impl. Closes #30

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Services/Map/PetTypeMapService.java
@@ -0,0 +1,42 @@
+package org.pet.clinic.data.Services.Map;
+
+import java.util.Set;
+
+import org.pet.clinic.data.Model.PetType;
+import org.pet.clinic.data.Services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+	
+}
---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Services/PetTypeService.java
@@ -0,0 +1,7 @@
+package org.pet.clinic.data.Services;
+
+import org.pet.clinic.data.Model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
