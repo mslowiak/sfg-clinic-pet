eec0701e5b63563edb890edf8f9607fe05e4d73e
Himovic <chabannehichem@gmail.com>
2019-09-20 15:54:51 +0200

    Create Visit Entity and update owner with pet relation. Closes #27

--- pet-clinic-data/src/main/java/org/pet/clinic/data/Model/Owner.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Model/Owner.java
@@ -1,5 +1,26 @@
 package org.pet.clinic.data.Model;
 
-public class Owner extends Person {
+import java.util.Set;
 
+public class Owner extends Person {
+	
+	private Set<Pet> setPets;
+	
+	public Owner() {
+		
+	}
+	
+	public Owner(Set<Pet> setPets) {
+		this.setPets = setPets;
+	}
+
+	public Set<Pet> getSetPets() {
+		return setPets;
+	}
+
+	public void setSetPets(Set<Pet> setPets) {
+		this.setPets = setPets;
+	}
+	
+	
 }
