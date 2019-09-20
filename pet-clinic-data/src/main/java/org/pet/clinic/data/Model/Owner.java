09c69c8ec6c2d539641f27bf737b3fcf28ec064e
Himovic <chabannehichem@gmail.com>
2019-09-20 16:07:07 +0200

    Enhance owner with Contact Info Properties. Closes #29

--- pet-clinic-data/src/main/java/org/pet/clinic/data/Model/Owner.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Model/Owner.java
@@ -5,13 +5,19 @@ import java.util.Set;
 public class Owner extends Person {
 	
 	private Set<Pet> setPets;
+	private String address;
+	private String city;
+	private String telephone;
 	
 	public Owner() {
 		
 	}
 	
-	public Owner(Set<Pet> setPets) {
+	public Owner(Set<Pet> setPets, String address, String city, String telephone) {
 		this.setPets = setPets;
+		this.address = address;
+		this.city = city;
+		this.telephone = telephone;
 	}
 
 	public Set<Pet> getSetPets() {
@@ -20,6 +26,30 @@ public class Owner extends Person {
 
 	public void setSetPets(Set<Pet> setPets) {
 		this.setPets = setPets;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
 	}
 	
 	
