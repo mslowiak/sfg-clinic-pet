adebb07516cf19305a932a6b52174e3c28c2aa04
Himovic <chabannehichem@gmail.com>
2019-09-26 11:14:50 +0200

    Create Visit Entity JPA and Associate it to Pet Entity. Closes #39.

--- pet-clinic-data/src/main/java/org/pet/clinic/data/Model/Pet.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Model/Pet.java
@@ -1,11 +1,15 @@
 package org.pet.clinic.data.Model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,8 +26,9 @@ public class Pet extends BaseEntity {
 	private Owner owner;
 	@Column(name = "birthday")
 	private LocalDate birthDate;
-	
-	public Pet() {
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
+ 	public Pet() {
 		
 	}
 	
@@ -65,6 +70,14 @@ public class Pet extends BaseEntity {
 
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+	
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 	@Override
