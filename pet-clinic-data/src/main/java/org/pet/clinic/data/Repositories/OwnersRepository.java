65fcebdbc52bd2c3f02568f5a5a69bfeb6d84152
Himovic <chabannehichem@gmail.com>
2019-09-26 11:50:45 +0200

    Create Spring Data JPA Service for Owners. Closes #41

--- pet-clinic-data/src/main/java/org/pet/clinic/data/Repositories/OwnersRepository.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Repositories/OwnersRepository.java
@@ -4,5 +4,7 @@ import org.pet.clinic.data.Model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnersRepository extends CrudRepository<Owner, Long> {
-
+ 
+	//Generate findByLastName
+	Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Services/SpringDataJpa/OwnerSpringDataJpaService.java
@@ -0,0 +1,72 @@
+package org.pet.clinic.data.Services.SpringDataJpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.pet.clinic.data.Model.Owner;
+import org.pet.clinic.data.Repositories.OwnersRepository;
+import org.pet.clinic.data.Repositories.PetsRepository;
+import org.pet.clinic.data.Repositories.PetsTypeRepository;
+import org.pet.clinic.data.Services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSpringDataJpaService implements OwnerService {
+	
+	private final OwnersRepository ownersRepository;
+	private final PetsRepository petsRepository;
+	private final PetsTypeRepository petsTypeRepository;
+	
+	public OwnerSpringDataJpaService(OwnersRepository ownersRepository, PetsRepository petsRepository,
+			PetsTypeRepository petsTypeRepository) {
+		super();
+		this.ownersRepository = ownersRepository;
+		this.petsRepository = petsRepository;
+		this.petsTypeRepository = petsTypeRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> owners = new HashSet<>();
+		ownersRepository.findAll().forEach(owners::add);
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		// TODO Auto-generated method stub
+		return ownersRepository.findById(id).orElse(null);
+		/*Optional<Owner> optionalOwner = ownersRepository.findById(id);
+		if(optionalOwner.isPresent()) {
+			return optionalOwner.get();
+		}else {
+			return null;
+		}*/
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		// TODO Auto-generated method stub
+		return ownersRepository.save(object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		ownersRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownersRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		// TODO Auto-generated method stub
+		return ownersRepository.findByLastName(lastName);
+	}
+	
+	
+}
