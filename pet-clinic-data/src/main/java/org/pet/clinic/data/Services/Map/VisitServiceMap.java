c381a8f3146fc1bf787452c2c1dbbf6fd21caa44
Himovic <chabannehichem@gmail.com>
2019-09-26 12:53:44 +0200

    Create Visit Map Service. Closes #47

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Services/Map/VisitServiceMap.java
@@ -0,0 +1,41 @@
+package org.pet.clinic.data.Services.Map;
+
+import java.util.Set;
+
+import org.pet.clinic.data.Model.Visit;
+import org.pet.clinic.data.Services.VisitService;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+	
+	
+	@Override
+	public Set<Visit> findAll(){
+		return super.findAll();
+	}
+	
+	@Override
+	public Visit save(Visit object) {
+		if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null
+				|| object.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid Visit");
+		}
+		return super.save(object);
+	}
+	
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+	
+	@Override
+	public void delete(Visit object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+}
---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/Services/VisitService.java
@@ -0,0 +1,7 @@
+package org.pet.clinic.data.Services;
+
+import org.pet.clinic.data.Model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
