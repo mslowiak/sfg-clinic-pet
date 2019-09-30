efe90036e0157db7bd916ec5b9c034905dd3bc83
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-10-01 01:16:06 +0200

    Add entity scan and enable jpa repositories annotations

--- pet-clinic-web/src/main/java/org/pet/clinic/web/ClinicApplication.java
+++ pet-clinic-web/src/main/java/org/pet/clinic/web/ClinicApplication.java
@@ -2,9 +2,13 @@ package org.pet.clinic.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.pet.clinic")
+@EntityScan("org.pet.clinic.data.Model")
 @ComponentScan(basePackages = {"org.pet.clinic.data",
 		"org.pet.clinic.data.Model",
 		"org.pet.clinic.data.Repositories",
